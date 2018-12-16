04f65a21c7d0e116796792194c3e6c49b657c774
paultran1975 <45541034+paultran1975@users.noreply.github.com>
2018-12-16 11:11:46 -0800

    change path

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 @pytest.fixture(scope="class")
 def initialize_chrome_driver(request):
     print("initiating chrome driver")
-    driver = webdriver.Chrome("D:\Drivers\drivers\chromedriver.exe")
+    driver = webdriver.Chrome(".\lib\drivers\chromedriver.exe")
     # driver.get("https://google.com")
     driver.maximize_window()
     request.cls.driver = driver
