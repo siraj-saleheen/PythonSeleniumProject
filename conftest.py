1855c615c774403809121b04bc5e96072546b647
ssaleheen <ssaleheen@sony.com>
2018-12-16 10:28:48 -0800

    updated chrome driver path

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from selenium import webdriver
 @pytest.fixture(scope="class")
 def initialize_chrome_driver(request):
     print("initiating chrome driver")
-    driver = webdriver.Chrome("/Users/ssaleheen/virtualenvs/a-python-selenium-sample-project/a-python-selenium-sample-project/lib/python3.6/site-packages/selenium/chromedriver") #if not added in PATH
+    driver = webdriver.Chrome("./drivers/chromedriver") #if not added in PATH
     # driver.get("https://google.com")
     driver.maximize_window()
     request.cls.driver = driver
